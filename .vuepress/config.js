964998ba1cf28f0d22aaf966b36eb965540ba592
Robert Lin <robert@bobheadxi.dev>
2020-05-25 09:01:21 -0700

    add quick links

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -17,7 +17,36 @@ module.exports = {
     nav: [
       { text: 'Handbook', link: '/handbook/' },
       { text: 'Resources', link: '/resources/' },
-      { text: 'About', link: 'https://ubclaunchpad.com', target:'_blank' },
+      {
+        text: 'Links',
+        items: [
+          { text: 'About Us', link: 'https://ubclaunchpad.com', target:'_blank' },
+          { text: 'GitHub', link: 'https://ubclaunchpad.com/github' },
+          { text: 'Facebook', link: 'https://ubclaunchpad.com/facebook' },
+          { text: 'Instagram', link: 'https://ubclaunchpad.com/instagram' },
+          {
+            text: 'GitHub Repositories',
+            items: [
+              { text: 'Docs', link: 'https://github.com/ubclaunchpad/docs' },
+              { text: 'Ideas', link: 'https://github.com/ubclaunchpad/ideas/issues' },
+              { text: 'Design', link: 'https://github.com/ubclaunchpad/design' },
+              { text: 'Strategy', link: 'https://github.com/ubclaunchpad/strategy' },
+              { text: 'Leads', link: 'https://github.com/ubclaunchpad/leads' },
+              { text: 'Exec', link: 'https://github.com/ubclaunchpad/exec' },
+            ]
+          },
+          {
+            text: 'Google Drive',
+            items: [
+              { text: 'Projects', link: 'https://drive.google.com/drive/u/0/folders/18piFDBdAUuZAOf9xOgpf2_HBUuVNae0S' },
+              { text: 'Design', link: 'https://drive.google.com/drive/u/0/folders/1Zfe25r3D77hGdyMkj0tlxHNa-r7fAq1d' },
+              { text: 'Strategy', link: 'https://drive.google.com/drive/u/0/folders/0BwdNv1PZjDeXMkc1eDVNY1ZHT00' },
+              { text: 'Leads', link: 'https://drive.google.com/drive/u/0/folders/1hgPcUC_DrFMmzZ04pBSlZFig4v9AbTuv' },
+              { text: 'Exec', link: 'https://drive.google.com/drive/u/0/folders/10b_2H5EhPpJtdgNi7QizRhWC9Qtivr8L' },
+            ]
+          },
+        ]
+      },
     ],
     searchPlaceholder: 'Search...',
 
