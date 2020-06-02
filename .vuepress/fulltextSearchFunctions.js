b6deef3ea82e2ab83b6fc6a513ae32c2005e3934
Robert Lin <robert@bobheadxi.dev>
2020-06-02 16:21:39 -0700

    cleanup

--- .vuepress/fulltextSearchFunctions.js
+++ .vuepress/fulltextSearchFunctions.js
@@ -1,14 +1,13 @@
 import * as Fathom from 'fathom-client';
 
-const EXTENDED_SUGGESTIONS_TITLE = 'Other';
-
 // This function extends suggestions provided by the fulltext-search plugin
-export async function processSuggestions(suggestions, queryString, queryTerms) {
+export async function processSuggestions(suggestions, queryString) {
   if (queryString) {
+    // these suggestions help users quickly jump to search on other relevant platforms
     suggestions.push({
       path: 'https://sourcegraph.com/search?patternType=literal&q=repo:^github.com/ubclaunchpad/*+',
       slug: queryString,
-      parentPageTitle: EXTENDED_SUGGESTIONS_TITLE,
+      parentPageTitle: 'Other',
       title: 'Content and code',
       contentStr: 'Search our repositories',
       external: true,
@@ -25,12 +24,11 @@ export async function processSuggestions(suggestions, queryString, queryTerms) {
 
 // view these in fathom - https://app.usefathom.com/share/oemmhhle/docs.ubclaunchpad.com
 const GOAL_SITESEARCH = 'SJ8NSKV1';
-const GOAL_OTHERSEARCH = 'VMCQLNMK';
+const GOAL_EXTENDEDSEARCH = 'VMCQLNMK';
 
 export async function onGoToSuggestion(i, suggestion) {
-  console.log(suggestion);
-  if (suggestion.parentPageTitle === EXTENDED_SUGGESTIONS_TITLE) {
-    Fathom.trackGoal(GOAL_OTHERSEARCH, 0);
+  if (suggestion.external === true) {
+    Fathom.trackGoal(GOAL_EXTENDEDSEARCH, 0);
   } else {
     Fathom.trackGoal(GOAL_SITESEARCH, 0);
   }
