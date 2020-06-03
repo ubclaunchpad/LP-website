2cf7a086b355c9fc8b0a898edf102dcad2516546
Robert Lin <robert@bobheadxi.dev>
2020-06-03 08:44:12 -0700

    bump fathom

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -112,9 +112,7 @@ module.exports = {
     // analytics are under team@ubclaunchpad.com, property docs.ubclaunchpad.com
     // dashboard: https://app.usefathom.com/share/oemmhhle/docs.ubclaunchpad.com
     ['@ubclaunchpad/fathom', {
-      // TODO: replace with https://chinchilla.ubclaunchpad.com/script.js
-      // once https://github.com/unstacked/fathom-client/issues/5 is closed
-      'fathomURL': 'https://chinchilla.ubclaunchpad.com/tracker.js',
+      'fathomURL': 'https://chinchilla.ubclaunchpad.com/script.js',
       'siteID': 'OEMMHHLE',
     }],
   ],
