b4ad6b3e19bb829ceefa07ed839232e4453ff2c1
hriday330 <hriday2004@outlook.com>
2024-08-06 15:32:08 -0700

    Created imageArea component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Navbar from '@/components/general/navbar';
 import { nunitoSans } from './fonts';
 import InfoButton from '@/components/buttons/infoButton';
+import ImageArea from '@/components/general/imageArea';
+
 const text = {
   heroTitle: "Welcome to UBC Launch Pad",
   heroDescription: "A student-run software engineering team devoted to building software projects in a collaborative and professional environment",
+  aboutUsTitle: "What we do at",
+  aboutUsSubtitle: "UBC Launch Pad",
+  aboutUsText: 'As the leading technology club at UBC, Launch Pad is devoted to create a collaborative and professional environment for software development. It is our goal to provide the best space for students to apply and develop their technical skills outside of classroom, to learn and practice industry-standard tools, and to build passion projects with like-minded individuals. In Launch Pad form into teams to work on an 8 month development project  based on their interests and skill sets. Each team consists of a  tech-lead, developers and designers, where they collaboratively go  through the design thinking process to design and build a product.'
+}
 
+const lpImageProps = {
+    src: "/images/launchpadTeam.png",
+    alt: "Launchpad team posing for a photo",
+    width: 998*3,
+    height: 580*3,
 }
 
 export default function Home() {
@@ -17,15 +28,13 @@ export default function Home() {
       </section>
 
       <section className="flex flex-row items-center">
-        <div className='flex flex-col items-center px-20 py-10 bg-white'>
-          <p>{'Picture goes here. At [Team/Organization Name], we embark on a journey of creation and collaboration. From brainstorming innovative ideas to executing complex coding tasks, we combine our technical prowess with a passion for learning. Our projects a rapidly evolving technological landscape.At [Team/Organization At [Team/Organization Name], we embark on a journey of creation and collaboration. Fro'}</p>
-        </div>
+        <ImageArea {...lpImageProps}/>
         <div className='flex flex-col items-end py-10 px-10'>
           <InfoButton text={'About us'}/>
-          <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans pt-5`}>{'What we do at'}</h1>
-          <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans text-indigo-400`}>{'Launch Pad'}</h1>
+          <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans pt-5`}>{text.aboutUsTitle}</h1>
+          <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans text-indigo-400`}>{text.aboutUsSubtitle}</h1>
           <p className='text-stone-400 text-right py-10'>
-            {'At [Team/Organization Name], we embark on a journey of creation and collaboration. From brainstorming innovative ideas to executing complex coding tasks, we combine our technical prowess with a passion for learning. Our projects a rapidly evolving technological landscape.At [Team/Organization At [Team/Organization Name], we embark on a journey of creation and collaboration. Fro'}
+            {text.aboutUsText}
           </p>
         </div>
       </section>
