f89ef6bc7073f30dfbd31b79f434a7a1148f8c4f
hriday330 <hriday2004@outlook.com>
2024-08-06 15:32:32 -0700

    Created join us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,10 @@ const text = {
   heroDescription: "A student-run software engineering team devoted to building software projects in a collaborative and professional environment",
   aboutUsTitle: "What we do at",
   aboutUsSubtitle: "UBC Launch Pad",
-  aboutUsText: 'As the leading technology club at UBC, Launch Pad is devoted to create a collaborative and professional environment for software development. It is our goal to provide the best space for students to apply and develop their technical skills outside of classroom, to learn and practice industry-standard tools, and to build passion projects with like-minded individuals. In Launch Pad form into teams to work on an 8 month development project  based on their interests and skill sets. Each team consists of a  tech-lead, developers and designers, where they collaboratively go  through the design thinking process to design and build a product.'
+  aboutUsText: 'As the leading technology club at UBC, Launch Pad is devoted to create a collaborative and professional environment for software development. It is our goal to provide the best space for students to apply and develop their technical skills outside of classroom, to learn and practice industry-standard tools, and to build passion projects with like-minded individuals. In Launch Pad form into teams to work on an 8 month development project  based on their interests and skill sets. Each team consists of a  tech-lead, developers and designers, where they collaboratively go  through the design thinking process to design and build a product.',
+  joinUsTitle: "Want to grow your skill?",
+  joinUsSubtitle: "Join our team!",
+  joinUsText: "We are looking for students passionate about tech with varying levels of experience to join our teams! "
 }
 
 const lpImageProps = {
@@ -18,6 +21,12 @@ const lpImageProps = {
     height: 580*3,
 }
 
+const memberRoles = [{
+  icon: "/images/launchpadTeam.png",
+  title: "Launchpad team posing for a photo",
+  text: "",
+  height: 580,
+}]
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
@@ -29,7 +38,7 @@ export default function Home() {
 
       <section className="flex flex-row items-center">
         <ImageArea {...lpImageProps}/>
-        <div className='flex flex-col items-end py-10 px-10'>
+        <div className='flex flex-col items-end py-10 pl-10 pr-12'>
           <InfoButton text={'About us'}/>
           <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans pt-5`}>{text.aboutUsTitle}</h1>
           <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans text-indigo-400`}>{text.aboutUsSubtitle}</h1>
@@ -37,6 +46,16 @@ export default function Home() {
             {text.aboutUsText}
           </p>
         </div>
+      </section>
+      <section className="flex flex-row items-center">
+        <div className='flex flex-col items-start py-10 pl-10 pr-12'>
+          <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans pt-5`}>{text.joinUsTitle}</h1>
+          <h1 className={`text-4xl font-bold ${nunitoSans.variable} font-sans text-indigo-400`}>{text.joinUsSubtitle}</h1>
+          <p className='text-stone-400 text-left py-10 w-10/12'>
+            {text.joinUsText}
+          </p>
+        </div>
+        <ImageArea {...lpImageProps}/>
       </section>
     </main>
   );
