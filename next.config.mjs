97666abae382355b4ddbdd45befe6044dcca721c
Armin Talaie <talaiearmin78@gmail.com>
2024-08-25 12:38:09 -0700

    nextjs build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
