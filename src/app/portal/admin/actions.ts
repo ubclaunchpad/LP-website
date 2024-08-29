cdffcf48accaca1f1c6001a3aac234578d7baf71
Armin Talaie <talaiearmin78@gmail.com>
2024-08-29 15:16:34 -0700

    prelim stats for forms (#353)

--- src/app/portal/admin/actions.ts
+++ src/app/portal/admin/actions.ts
@@ -2,18 +2,23 @@
 import { db } from "@/db";
 import {FormStep} from "@/lib/types/questions";
 import {FormFields} from "@/app/portal/admin/forms/[id]/submissions/columns";
+import {ChartConfig} from "@/components/primitives/chart";
+
+
+const inMemoryCache = new Map();
+inMemoryCache.set("forms", {});
 
 export async function getForms() {
   return db.forms.findMany();
 }
 
-export async function getUsers() {
-  const users = await db.users.findMany();
-  return users.reduce((acc: Record<string, any>, user: any) => {
-    acc[user.id] = { ...user, name: user["raw_user_meta_data"]?.full_name };
-    return acc;
-  }, {});
-}
+// export async function getUsers() {
+//   const users = await db.users.findMany();
+//   return users.reduce((acc: Record<string, any>, user: any) => {
+//     acc[user.id] = { ...user, name: user["raw_user_meta_data"]?.full_name };
+//     return acc;
+//   }, {});
+// }
 
 export async function createForm(data: { title: string; description: string }) {
   return db.forms.create({ data: { ...data, config: {}, questions: [] } });
@@ -44,13 +49,16 @@ export async function updateForm(
   return db.forms.update({ where: { id }, data });
 }
 
-export async function getSubmissions(formId: number) {
+export async function getSubmissions(formId: number, onlySubmitted = true) {
   const app = await db.submissions.findMany({
     include: {
       users: true,
       applications: true,
     },
-    where: { form_id: BigInt(formId), status: { not: "pending" } },
+    where: { form_id: BigInt(formId),
+    ...(onlySubmitted && {
+        status: { not: "pending" }})
+    },
   });
   return app.map((submission: any) => {
     const details = submission.details ? (submission.details as any) : {};
@@ -101,4 +109,113 @@ export async function getAllFormDetails(formId: bigint): Promise<{ rawForm: any,
         console.log(e);
         return { rawForm: null, formFields: {}, submissions: [] };
     }
+}
+
+type temp = {
+    columns: string[],
+}
+
+
+interface AggregatedValue {
+    id: string;
+    count: number;
+    label: string;
+}
+
+interface AggregationResult {
+    charInfo: {
+        title: string;
+        description: string;
+    };
+    chartData: AggregatedValue[];
+    chartConfig: any; // Replace 'any' with the appropriate type if you know it
+}
+
+function aggregateColumn(data: Record<string, any>[], column: string): AggregationResult {
+    const aggData = data.reduce((acc: Record<string, AggregatedValue>, row: Record<string, any>) => {
+        const value = row[column];
+        if (!value) {
+            return acc;
+        }
+        if (Array.isArray(value)) {
+            value.forEach((val) => {
+                if (acc[val]) {
+                    acc[val] = {
+                        ...acc[val],
+                        count: acc[val].count + 1,
+                    };
+                } else {
+                    acc[val] = {
+                        id: val,
+                        count: 1,
+                        label: val,
+                    };
+                }
+            });
+            return acc;
+        }
+        if (acc[value]) {
+            acc[value] = {
+                ...acc[value],
+                count: acc[value].count + 1,
+            };
+        } else {
+            acc[value] = {
+                id: value,
+                count: 1,
+                label: value,
+            };
+        }
+        return acc;
+    }, {} as Record<string, AggregatedValue>);
+
+    return {
+        charInfo: { title: column, description: `Number of ${column}(s)` },
+        chartData: Object.values(aggData),
+        chartConfig: createChartConfig(Object.values(aggData), [column]), // Type 'chartConfig' properly
+    };
+}
+
+function createChartConfig(data: any[], columns: string[]) {
+  const chartConfig: ChartConfig = {};
+  columns.forEach((column, index) => {
+    chartConfig[column] = {
+      label: column,
+      color: `var(--lp-200)`,
+    };
+  });
+  return chartConfig;
+}
+
+export async function getFormAnalytics(formId: number, config: temp) {
+  const cachedData = inMemoryCache.get("forms");
+  if (cachedData[formId]) {
+      const formAnalytics = cachedData[formId].analyticsData;
+      const expiryDate = cachedData[formId].expiryDate;
+      if ( expiryDate > Date.now()) {
+          return {analyticsData:formAnalytics, stats: cachedData[formId].stats};
+      }
+  }
+
+  const [submissions, form]= await  Promise.all([getSubmissions(formId),getForm(formId)]);
+      if (!form) {
+     throw new Error("No form found");
+        }
+  const chartData = config.columns.map((column) => aggregateColumn(submissions, column));
+  const lastUpdated = Date.now();
+    inMemoryCache.set("forms", {
+        [formId]: {
+        analyticsData: chartData,
+        expiryDate: Date.now() + 1000 * 60 * 60 , // 1 hour
+        columns: config.columns,
+        stats : {
+            submissions: submissions.length,
+            lastUpdated: lastUpdated,
+        }
+        },
+    });
+  return {  analyticsData: chartData , stats: {
+    submissions: submissions.length,
+      lastUpdated: lastUpdated,}
+  }
 }
---
+++ src/app/portal/admin/forms/[id]/analytics/formDataChart.tsx
@@ -0,0 +1,82 @@
+"use client"
+import { Bar, BarChart, XAxis, YAxis } from "recharts"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/primitives/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/primitives/chart"
+
+
+export function FormChart(
+    {
+        chartInfo,
+        chartConfig,
+        chartData,
+    }: {
+        chartInfo: { title: string, description: string },
+        chartConfig: ChartConfig,
+        chartData: { id: string, label: string, count: number, fill?: string }[],
+    }
+
+) {
+
+    const colouredData = chartData.map((data, index) => {
+        return {
+            ...data,
+            fill: `var(--lp-${((index + 1)*100 % 500) + 400})`,
+        }
+    });
+
+    return (
+        <Card className={"max-w-2xl flex-1 w-full "}>
+            <CardHeader>
+                <CardTitle>{chartInfo.title}</CardTitle>
+                <CardDescription>{chartInfo.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart
+                        accessibilityLayer
+                        data={colouredData}
+                        layout="vertical"
+                        margin={{
+                            left: 0,
+                        }}
+                    >
+                        <YAxis
+                            className={"min-w-80 *:text-white! max-w-none "}
+                            dataKey="label"
+                            type="category"
+                            tickLine={false}
+                            stroke={"var(--background-100)"}
+                            tickMargin={10}
+                            fill={"#ffffff"}
+                            width={150}
+                            axisLine={true}
+                            tickFormatter={(value) =>
+                                value.length > 20 ? value.slice(0, 20) + "..." : value
+                            }
+                        />
+                        <XAxis dataKey="count" type="number" hide />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent  />}
+                        />
+                        <Bar dataKey="count" layout="vertical" radius={2} />
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col items-start gap-2 text-sm">
+            </CardFooter>
+        </Card>
+    )
+}
