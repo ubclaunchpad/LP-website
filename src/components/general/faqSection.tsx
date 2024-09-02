18f7c8a277e6c67e525299721ee67d3cf0722295
Arman Moztarzadeh <armanmoztar@outlook.com>
2024-09-01 18:19:26 -0700

    fix: faq text cut off clicked from nav

--- src/components/general/faqSection.tsx
+++ src/components/general/faqSection.tsx
@@ -41,14 +41,14 @@ export default function FaqSection({ faqs }: FaqSectionProps) {
   return (
     <div className="flex flex-col items-center py-10 pt-20">
       <h2
-        className="text-3xl text-heading font-[600] text-[48px] text-white "
+        className="text-3xl text-heading font-[600] text-[48px] text-white"
         id={"faq"}
       >
         Frequently Asked Questions
       </h2>
       <div className="max-w-7xl mx-auto py-20 px-4 sm:py-24 sm:px-6 lg:px-8">
         <dl className="grid grid-cols-1 gap-y-6 lg:grid-cols-2 sm:gap-x-8 sm:gap-y-8">
-          <div className="sm:col-span-1 gap-y-6 flex flex-col ">
+          <div className="sm:col-span-1 gap-y-6 flex flex-col">
             {makeFAQColumn(faqs, 0, Math.ceil(faqs.length / 2))}
           </div>
           <div className="sm:col-span-1 gap-y-6 flex flex-col">
@@ -67,7 +67,7 @@ function QuestionAccordion({
   setOpenIndex,
 }: QuestionAccordionProps) {
   return (
-    <div className="bg-[#3A3543] py-[20px] px-[20px] h-fit rounded-lg ">
+    <div className="bg-[#3A3543] py-[20px] px-[20px] h-fit rounded-lg border border-purple-300">
       <dt className="text-lg">
         <button
           className="text-left w-full px-2.5 flex justify-between items-start text-white"
