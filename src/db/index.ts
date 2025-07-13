783cd9a4bfe1c36deb1bfa5f1f32671c5a40754f
TonyLiu0226 <TL0226yn@gmail.com>
2025-07-12 20:27:20 -0700

    prisma config for edge runtime

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+// Extend the NodeJS.Global interface to include prisma
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 let db: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
+// Ensure process.env.DATABASE_URL is defined for production
+if (process.env.NODE_ENV === "production" && process.env.DATABASE_URL) {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool as any);
+  db = new PrismaClient({ adapter: adapter as any });
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
